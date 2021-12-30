06cc1946201222c9ca048962736cbafa4fd8a9be
brk-a <fnjakai@outlook.com>
2021-12-30 14:22:05 +0000

    Simple game on website: Reaction timer

---
+++ 0x00-Learn_Vue/2-test_website/reaction-timer/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
+  <Result v-if="showResult" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
+
+export default {
+  name: 'App',
+  components: { Block, Result},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResult: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 1000 + Math.random() * 2000
+      this.isPlaying = true
+      console.log(this.delay)
+      this.showResult = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults()
+    },
+    showResults() {
+      this.showResult = true
+    },
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+button {
+  background: #0faf87;
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button [disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ 0x00-Learn_Vue/2-test_website/reaction-timer/src/components/Result.vue
@@ -0,0 +1,32 @@
+<template>
+  <p class="result">Reaction time: {{ score }} ms</p>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 150) {
+            this.rank = 'Fancy Fingers!'
+        } else if (this.score < 300) {
+            this.rank = 'Rapid Reflexes'
+        } else {
+            this.rank = 'Snail\'s Pace...'
+        }
+    },
+}
+</script>
+
+<style>
+.rank{
+    font-size: 1.4em;
+    color: #0faf87;
+    font-weight: bold;
+}
+</style>
