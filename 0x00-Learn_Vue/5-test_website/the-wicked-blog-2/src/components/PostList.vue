2b64d8f93bde67fe7aa006063ffe59dc155d7c8b
brk-a <fnjakai@outlook.com>
2022-01-02 14:10:29 +0000

    The Wicked Blog: Simple website with data hard-coded w/i a ref

---
+++ 0x00-Learn_Vue/5-test_website/the-wicked-blog-2/src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="post-list">
+      <div v-for="post in posts" :key="post.id">
+          <SinglePost :post="post"/>
+      </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
+import SinglePost from './SinglePost.vue'
+
+export default {
+    components: {SinglePost},
+    props: ['posts'],
+    setup(props) {
+        onMounted (() => console.log('PostList has mounted'))
+        onUnmounted(() => console.log('PostList has unmounted'))
+        onUpdated(() => console.log('PostList is updated'))
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 0x00-Learn_Vue/5-test_website/the-wicked-blog-2/src/views/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <PostList :posts="posts" v-if="showPosts"/>
+    <button @click="showPosts = !showPosts">Posts</button>
+    <button @click="posts.pop()">Delete Post</button>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+import PostList from '../components/PostList.vue'
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {PostList},
+  setup() {
+    const posts = ref([
+      {title: 'Welcome to The Wicked Blog', 
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      id: 1},
+      {title: 'Why Vue?',
+      body: 'Short answer: because Vue. Long answer: lorem ipsum dolor sit amet, consectetur adipiscing elit,\
+      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\
+      Morbi tristique senectus et netus et. Orci dapibus ultrices in iaculis \
+      nunc sed augue lacus viverra. Senectus et netus et malesuada fames ac\
+      turpis egestas. Maecenas pharetra convallis posuere morbi.',
+      id: 2}
+    ])
+
+    const showPosts = ref(false)
+
+    return {posts, showPosts}
+  }
+}
+</script>
